3f94e5c04645b091e781f6c578a7f3078b06c67c
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2023-12-04 09:36:20 +0100

    Changement de script pour la gestion des modales et fin de l'utilisation de JQuery
    Signed-off-by: Jean-Baptiste-Paris <ecrireajb@gmail.com>

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,21 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
+// $(document).ready(function() {
+//     $('.gallery').mauGallery({
+//         columns: {
+//             xs: 1,
+//             sm: 2,
+//             md: 3,
+//             lg: 3,
+//             xl: 3
+//         },
+//         lightBox: true,
+//         lightboxId: 'myAwesomeLightbox',
+//         showTags: true,
+//         tagsPosition: 'top'
+//     });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.gallery').mauGallery({
         columns: {
             xs: 1,
             sm: 2,
